40694bdf20738bc7a196e3dce0ac78311a3d1604
jay <jayjain9@gmail.com>
2018-10-16 15:48:41 +0530

    [install]

---
+++ demo/src/main/java/com/example/demo/controler/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controler;
+
+import java.io.IOException;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+
+@RestController 
+public class HomeController {
+	@RequestMapping(value = "shop", method=RequestMethod.GET)
+	public String home() throws InvalidFormatException, IOException {
+		//ModelAndView modelAndView = new ModelAndView();
+		//modelAndView.addObject("key", "home");
+		//modelAndView.setViewName("home");
+		return "hello1";
+	} 
+}
